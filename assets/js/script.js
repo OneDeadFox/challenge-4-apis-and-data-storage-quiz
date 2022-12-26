c269960e60a0ca9cb63be3093c2485d630093343
Tyler Brown <tjosephb.pro@gmail.com>
2022-12-26 15:48:03 -0800

    added start button apis, and timer functions for start screen fade/game play timer. Began working on question objects and question population function

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,73 @@
 //TODO: allow player to save initials and score to leader board
     //1. have a submit button that addes users initials and score to a database that wont be erased on refresh or esc
 
+//Global Variables
+let pageEl = document.body;
+let timerEl = document.querySelector("#timer")
+let startScreenEl = document.getElementById("veil")
+let opacity = 100;
+let gameTime = 300;
+
+
+
+//#region Event Delegator
+//Delegator
+
+pageEl.addEventListener("click", function(event){
+    var element = event.target;
+
+    if(element.matches("#start")){
+        //function that will reduce id-veil opacity in 1% increments over a period of time until it reaches 100%
+        screenTimer();
+    }
+
+});
+
+    //Delegator Functions
+    function screenTimer(){
+        var screenTimeLeft = setInterval(function() {
+            opacity--;
+            //make startScreen disappear
+            startScreenEl.setAttribute("style", `opacity: ${opacity}%`);
+            //clear timer and display upon endtime
+            if(opacity <= 0) {
+                clearInterval(screenTimeLeft);
+                startScreenEl.setAttribute("style", "display: none");
+                gameTimer();
+            }
+        },2);
+    }
+    function gameTimer(){
+        var gameTimeLeft = setInterval(function() {
+            gameTime--
+            //Count down time on screen
+            timerEl.textContent = gameTime + " sec";
+            if(gameTime <= 0) {
+                //TODO: make this function
+                //endGame();
+                clearInterval(gameTimeLeft);
+            }
+        }, 1000);
+    }
+
+//#endregion Event Delegator
+
+
+//#region Objects
+let question1 = {
+    question: "this is a test question",
+    number: 1,
+    answers: {answer1: "a",
+            answer2: "b",
+            answer3: "c",
+            answer: "d"
+            }
+};
+//#endregion Objects
+
+
+//#region Functions
+function questionPop(){
+    
+}
+//#endregion Functions
