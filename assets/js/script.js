2755446c0e6e2480abc2a5efb142ca89c376abc1
Tyler Brown <tjosephb.pro@gmail.com>
2022-12-28 18:19:19 -0800

    added api's and functions for random question/associated answers generation and population.

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,12 +37,13 @@
 
 //Global Variables
 let pageEl = document.body;
-let timerEl = document.querySelector("#timer")
-let startScreenEl = document.getElementById("veil")
+let timerEl = document.querySelector("#timer");
+let startScreenEl = document.getElementById("veil");
+let formEl = document.getElementById("question-block");
 let opacity = 100;
 let gameTime = 300;
-
-
+let questionSet = [];
+let answerSet = [];
 
 //#region Event Delegator
 //Delegator
@@ -88,20 +89,119 @@ pageEl.addEventListener("click", function(event){
 
 
 //#region Objects
+
+//change answers to objects with id, answer string, and validity properties.
 let question1 = {
-    question: "this is a test question",
-    number: 1,
-    answers: {answer1: "a",
-            answer2: "b",
-            answer3: "c",
-            answer: "d"
-            }
+    id: "question",
+    question: "This is a test question",
+    number: 0,
+    answers: [{id: "answer-1", number: 0, answer: "a", validity: false}, {id: "answer-2", number: 0, answer: "b", validity: false}, {id: "answer-3", number: 0, answer: "c", validity: false}, {id: "answer-4", number: 0, answer: "d", validity: false}]
 };
+
+let question2 = {
+    id: "question",
+    question: "This is a another test",
+    number: 0,
+    answers: [{id: "answer-1", number: 0, answer: "1", validity: false}, {id: "answer-2", number: 0, answer: "2", validity: false}, {id: "answer-3", number: 0, answer: "3", validity: false}, {id: "answer-4", number: 0, answer: "4", validity: false}]};
+
+let question3 = {
+    id: "question",
+    question: "This is the last test question",
+    number: 0,
+    answers: [{id: "answer-1", number: 0, answer: "i", validity: false}, {id: "answer-2", number: 0, answer: "ii", validity: false}, {id: "answer-3", number: 0, answer: "iii", validity: false}, {id: "answer-4", number: 0, answer: "iv", validity: false}]};
 //#endregion Objects
 
 
 //#region Functions
-function questionPop(){
+function questionPop() {
+    var questionFill = document.querySelector("#question");
+    var answerOptions = document.querySelectorAll('[name="answer"]');
+    var questionList = [question1, question2, question3];
+    var lineup = [];
+
+    //create array of random number order
+    function lineupRandomizer(...arr) {
+        for(var i = 0; i <= arr.length; i++){
+            var tempNum = Math.floor(Math.random()*arr.length);
+            //see if array have generated value
+            if(lineup.includes(tempNum)){
+                i--;
+            }else{
+            lineup.push(tempNum);
+            }
+
+            //set numbers in questionLineup array and exit loop
+            //questionList only
+            if(lineup.length === arr.length && arr[0].question != undefined) {
+                //fill arr object numbers with newly assigned values
+                for(i = 0; i < lineup.length; i ++) {
+                    arr[i].number = lineup[i];
+                }
+                //var ii set for following for loop
+                var ii = 0;
+                //set questions in new array in new order
+                for(i = 0; i < arr.length; i ++) {
+                    if(arr[ii].number === i) {
+                        questionSet[i] = arr[ii];
+                    ii = 0;
+                    } else {
+                        i--;
+                        ii++;
+                    }
+                }
+                //Populate question based on lineup arr.
+                questionFill.textContent = questionSet[0].question;
+                return;
+            //set up answers in a random ordered array
+            //answers only
+            } else if(lineup.length === arr.length && arr[0].question === undefined) {
+                //set answers in an array in a new order
+                //fill arr object numbers with newly assigned values
+                for(i = 0; i < lineup.length; i ++) {
+                    arr[i].number = lineup[i];
+        
+                }
+                //var ii set for following for loop
+                var ii = 0;
+                //set questions in new array in new order
+                for(i = 0; i < arr.length; i ++) {
+                    if(arr[ii].number === i) {
+                        answerSet[i] = arr[ii];
+                        console.log([arr[ii].number]);
+                        console.log(arr[ii].id)
+                    ii = 0;
+                    } else {
+                        i--;
+                        ii++;
+                    }
+                }
+
+                for(var i = 0; i < answerOptions.length; i++){
+                    var label = document.createElement("label");
+                    var linebreak = document.createElement("br");
+                    label.setAttribute("for", `answer-${answerSet[i].number}`);
+                    label.setAttribute("class", "label");
+                    label.setAttribute("id", `label-${answerSet[i].number}`);
+                    label.textContent = answerSet[i].answer;
+                    answerOptions[i].after(label);
+                    var labels = document.querySelectorAll(".label");
+                    console.log(labels[i]);
+                    labels[i].after(linebreak);
+                    console.log(label);
+                    }
+
+                return;
+
+            }
+        }
+    }
+
+    lineupRandomizer(...questionList);
     
+    lineupRandomizer(...questionSet[0].answers);
 }
+
+
 //#endregion Functions
+
+questionPop();
